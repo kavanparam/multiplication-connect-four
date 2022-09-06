7a2b7b65feea84e6646700e19c5d11b07d57bd2c
kavanparam <kavan.param@gmail.com>
2022-09-06 00:14:23 -0400

    Commit with subbranch
    - bunch of changes that I stashed in mainSubBranch
    - make a change to mainSubBranch next

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import logo from './logo.svg';
+import './App.css';
+
+function App(){
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className='text-3xl text-center'>Welcome to Multiplication Connect Four</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
