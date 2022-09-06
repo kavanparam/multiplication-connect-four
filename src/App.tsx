54e6b31f878f97583b9ea471c8fc36ce1447d8f5
kavanparam <kavan.param@gmail.com>
2022-09-06 00:24:54 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App(){
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1 className='text-3xl text-center'>Welcome to Multiplication Connect Four</h1>
-        <p>This is a change</p>
+        <p>This is another change</p>
       </header>
     </div>
   );
